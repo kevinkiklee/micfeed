ea0b7a62f3bdbd7762bb01b22ef09ef2a6a000a5
kevinkiklee <kevin.kik.lee@gmail.com>
2017-04-14 14:38:59 -0400

    Create initial cookie

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import cookie from 'react-cookie';
 import configureStore from './store/store';
 
 import Root from './Root.js';
@@ -9,6 +10,10 @@ import './index.css';
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
+
+  if (!cookie.load('sortedBy')) {
+    cookie.save('sortedBy', '');
+  }
 
   ReactDOM.render(<Root store={store}/>, root);
 });
