8023d9b01e29220e9063718817be784068004d73
kevinkiklee <kevin.kik.lee@gmail.com>
2017-04-17 11:38:33 -0400

    Add SortReducer to RootReducer

--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+
 import FeedReducer from './FeedReducer';
+import SortReducer from './SortReducer';
 
 const RootReducer = combineReducers({
   feed: FeedReducer,
+  sort: SortReducer,
 });
 
 export default RootReducer;
